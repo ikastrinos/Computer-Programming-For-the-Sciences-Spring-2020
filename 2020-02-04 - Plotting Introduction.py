b13d0a33506aea8729d12c215655cad5c5fa0c82
Brian Blais <bblais@gmail.com>
2020-02-04 12:47:06 -0500

    plotting

--- 2020-02-04 - Plotting Introduction.py
+++ 2020-02-04 - Plotting Introduction.py
@@ -11,6 +11,11 @@ get_ipython().run_line_magic('pylab', 'inline')
 # \frac{dy}{dt} = a+b\sin(c\cdot t)
 # $$
 # 
+# $$
+# dy  = \left(a+b\sin(c\cdot t) \right) \cdot dt
+# $$
+# 
+# 
 # Solution (https://www.wolframalpha.com/input/?i=integrate%28a%2Bb*sin%28c*t%29%2Ct%29):
 # 
 # $$
@@ -50,6 +55,62 @@ xlabel('time')
 ylabel('y')
 
 
+# In[5]:
+
+
+plot(t_list,y_list)
+xlabel('time [s]')
+ylabel('position [m]')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
 # Things I don't like about this plot:
 # 
 # * fonts too small
@@ -59,23 +120,22 @@ ylabel('y')
 # In[5]:
 
 
-def mydefaults():
-    from matplotlib import rcParams
-    
-    fontsize=20
-    
-    rcParams['font.size']=fontsize
-    rcParams['font.family']='sans-serif'
-    
-    rcParams['axes.labelsize']=fontsize
-    rcParams['axes.titlesize']=fontsize
-    rcParams['xtick.labelsize']=fontsize
-    rcParams['ytick.labelsize']=fontsize
-    rcParams['legend.fontsize']=fontsize
+from matplotlib import rcParams
 
-    rcParams['figure.figsize']=(12,8)
+fontsize=20
 
-    rcParams['axes.grid']=True
+rcParams['font.size']=fontsize
+rcParams['font.family']='sans-serif'
+
+rcParams['axes.labelsize']=fontsize
+rcParams['axes.titlesize']=fontsize
+rcParams['xtick.labelsize']=fontsize
+rcParams['ytick.labelsize']=fontsize
+rcParams['legend.fontsize']=fontsize
+
+rcParams['figure.figsize']=(12,8)
+
+rcParams['axes.grid']=True
 
 
 # In[6]:
