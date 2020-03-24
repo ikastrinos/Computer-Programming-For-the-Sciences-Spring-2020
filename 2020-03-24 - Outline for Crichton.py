e7e1fd6aa5786d00f310676131bf1182108bba95
Brian Blais <bblais@gmail.com>
2020-03-24 13:02:22 -0400

    small changes to crichton fit

---
+++ 2020-03-24 - Outline for Crichton.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Outline for reproducing Crichton's data analysis
+
+# In[ ]:
+
+
+1. Reproduce his plots
+    1. 
+
--- 2020-03-24 - Reading CSV and Regression.py
+++ 2020-03-24 - Reading CSV and Regression.py
@@ -17,21 +17,21 @@ from sci378 import *
 # 2. Looked down at the bottom for the search, and searched for "berkeley"
 # 3. then clicked the station name "Berkeley" hyperlink
 # 4. at the bottom, downloaded the CSV
-# 5. renamed the station.csv to berkeley.csv and put it in my data file
+# 5. renamed the station.csv to berkeley.csv and put it in my data folder
 
-# In[26]:
+# In[36]:
 
 
 data=pd.read_csv('data/berkeley.csv')
 
 
-# In[27]:
+# In[37]:
 
 
 data
 
 
-# In[28]:
+# In[39]:
 
 
 x=array(data['YEAR'])
@@ -39,19 +39,19 @@ y=array(data['metANN'])
 y[y==999.9]=nan
 
 
-# In[29]:
+# In[40]:
 
 
 x
 
 
-# In[30]:
+# In[41]:
 
 
 y
 
 
-# In[31]:
+# In[42]:
 
 
 station='BERKELEY'
@@ -61,7 +61,7 @@ ylabel('Temperature [C]')
 title(station)
 
 
-# In[32]:
+# In[43]:
 
 
 model=ols('y ~ x', data={'y':y,'x':x})
@@ -69,7 +69,7 @@ results=model.fit()
 results.summary()
 
 
-# In[33]:
+# In[44]:
 
 
 xx=linspace(min(x)-10,max(x)+10,20)
@@ -86,9 +86,32 @@ title(station+" : y=%.3g (+/- %.4g) x + %.3g" % (m,mσ,b))
 plot(xx,yy,'b-')
 
 
+# When uncertainty written like: 0.00481 +- 0.00125
+# 
+# * mean value of 0.00481
+# * 1-$\sigma$ uncertainty is 0.00125
+# * look at 95% range, then I look 2-$\sigma$ uncertainty
+
+# In[46]:
+
+
+v=0.00481
+one_σ=0.00125
+lower_bound=v - 2*one_σ
+upper_bound=v + 2*one_σ
+
+lower_bound,upper_bound
+
+
+# In[47]:
+
+
+0.00231*100 # per century
+
+
 # ## Save the plot, but don't show it
 
-# In[35]:
+# In[45]:
 
 
 plot(x,y,'-o')
